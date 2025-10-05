9760babc9e1cf62118e5e362dbe7d64d7d8b7d59
binail <Dima200602@yandex.ru>
2025-10-05 20:18:52 +0300

    add base logic

---
+++ Assets/Scripts/Utils/Extensions/ButtonExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace Utils.Extensions
+{
+    public static class ButtonExtensions
+    {
+        public static void SetOnClickAction(this Button button, Action onClick, bool playClickSound = true)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => onClick?.Invoke());
+        }
+    }
+}
